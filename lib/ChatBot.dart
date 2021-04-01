b631630b8aa45135aa7f6c6fe1871c108da9c664
malavsukhadia <malavsukhadia@gmail.com>
2021-04-01 21:42:26 +0530

    Added dependency of slider.

---
+++ lib/ChatBot.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
+class chatbot extends StatefulWidget {
+  @override
+  _chatbotState createState() => _chatbotState();
+}
+
+class _chatbotState extends State<chatbot> {
+  void response(query)async{
+    AuthGoogle authGoogle=await AuthGoogle(
+      fileJson: "images/medicalbot-dexe-69fe9e55151d.json"
+    ).build();
+    Dialogflow dialogflow= Dialogflow(authGoogle: authGoogle,language: Language.english);
+    AIResponse aiResponse= await dialogflow.detectIntent(query);
+    setState(() {
+      messages.insert(0,{"data":0,"message":aiResponse.getListMessage()[0]["text"]["text"][0].toString()});
+    });
+  }
+  final messageInsert=TextEditingController();
+  List<Map> messages=List();
+  Color secondaryColor = Color(0xff232c51);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: secondaryColor,
+        title:Center(
+          child: Text(
+            'Dr.X',
+          ),
+        )
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Flexible(
+                child: ListView.builder(
+                  reverse: true,
+                  padding: EdgeInsets.all(10.0),
+                  itemCount: messages.length,
+                    itemBuilder: (context,index)=>messages[index]["data"]==0?
+                    Text(messages[index]["message"].toString()):
+                    Text(messages[index]["message"].toString(),textAlign: TextAlign.right,)
+                )),
+            Divider(
+              height: 3.0,
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: 20.0),
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                children: <Widget>[
+                  Flexible(child: TextField(
+                    controller: messageInsert,
+                    decoration: InputDecoration.collapsed(hintText: 'Type your message'),
+                  )),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 4.0),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.send
+                      ),
+                      onPressed: (){
+                        if(messageInsert.text.isEmpty)
+                          {
+                            print('Empty mesaage');
+                          }else{
+                          setState(() {
+                            messages.insert(0,{"data":1,"message":messageInsert.text});
+                          });
+                          response(messageInsert.text);
+                          messageInsert.clear();
+                        }
+                      },
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
