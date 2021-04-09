81a29441929cf660613de9f1360d3d5eaba243c3
viraj-it <vkpatva1150@gmail.com>
2021-04-10 00:33:11 +0530

    demo.dart added

---
+++ lib/Constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kLableTextStyle = TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98));
+const kBottomContainerHeight = 70.0;
+const Color kActiveCardColor = Color(0xFF1D1E33);
+const Color kInactiveCardColor = Color(0xFF0A0E28);
+const Color kButtonColor = Color(0xFFEB1555);
+const kHeavyTextLable=TextStyle( fontSize: 50.0, fontWeight: FontWeight.w900);
+const kLargeButtonTextStyle=TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold );
+const kTitleTextStyle=TextStyle(fontSize: 50.0,fontWeight: FontWeight.bold );
+const kResultTextStyle=TextStyle(fontSize: 22.0,color: Color(0xFF24D876),fontWeight: FontWeight.bold);
+const kBMITextStyle=TextStyle(fontWeight: FontWeight.bold,fontSize: 100.0);
+const kInterpretation=TextStyle(fontSize: 22.0);
---
+++ lib/Gender_Card.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'Constants.dart';
+
+class GenderCard extends StatelessWidget {
+  GenderCard({@required this.text,this.icon});
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          text,
+          style: kLableTextStyle
+        )
+      ],
+    );
+  }
+}
